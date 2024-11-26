dca654829e8e17549a3eb8a3f956bb27b1597f9a
byeong98 <byeong98@danusys.com>
2024-11-26 18:02:31 +0900

    [2024.11.26] 임병영 - 6주차 알고리즘 과제 제출

---
+++ Algorithm/src/byeongyeong/week_6rd/BJ_1436.java
@@ -0,0 +1,20 @@
+package byeongyeong.week_6rd;
+
+/**
+ * 6주차 과제 : 1181(단어 정렬), 1436(영화 감독 숌), 1676(팩토리얼 0의 개수)
+ *
+ * 1436, 영화 감독 숌
+ * 출처: https://www.acmicpc.net/problem/1436
+ *
+ * 입력 : 첫째 줄에 N이 주어진다. N은 10,000보다 작거나 같은 자연수이다.
+ *
+ * 출력 : 첫째 줄에 N번째 영화의 제목에 들어간 수를 출력한다.
+ *
+ * @author byeongyeong, Lim
+ */
+public class BJ_1436 {
+
+    public static void main(String[] args) {
+
+    }
+}
